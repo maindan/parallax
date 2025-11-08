0a36ba241bb4c12051aef28839cc80a2137ae5bb
dan <danielkksilva1@gmail.com>
2025-11-08 13:07:23 -0400

    feat: eslint config and gallery add

--- eslint.config.js
+++ eslint.config.js
@@ -20,6 +20,8 @@ export default defineConfig([
       globals: globals.browser,
     },
     rules: {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "error",
       '@typescript-eslint/no-explicit-any': 'off',
     },
   },
--- src/pages/content/Content.tsx
+++ src/pages/content/Content.tsx
@@ -8,7 +8,9 @@ type ContentProps = {
 export default function Content({ scrollContainer }: ContentProps) {
   const bgRef = useRef<HTMLImageElement | null>(null);
   const containerRef = useRef<HTMLDivElement | null>(null);
-  const photoRef = useRef<HTMLImageElement | null>(null)
+  const photoRef = useRef<HTMLImageElement | null>(null);
+
+  const photos:string[] = ["man.jpg", "window.jpg", "brut.jpg", "desert.jpg", "arch.jpg"]
 
   useLayoutEffect(() => {
     if (!scrollContainer.current || !bgRef.current) return;
@@ -31,8 +33,8 @@ export default function Content({ scrollContainer }: ContentProps) {
 
     tl.fromTo(
       photoRef.current,
-      { width: "40%" },
-      { width: "50%" }
+      { width: window.innerWidth < 800 ? "40%" : "15%" },
+      { width: window.innerWidth < 800 ? "50%" : "20%" }
     )
 
     return () => {
@@ -63,13 +65,29 @@ export default function Content({ scrollContainer }: ContentProps) {
         <h2
           data-scroll
           data-scroll-speed="2"
-          className="absolute z-20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-6xl text-white"
+          className="absolute z-20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-5xl sm:text-6xl text-white"
         >
           PHOTOGRAPH
         </h2>
       </div>
-      <div className="w-full h-[140vh] pt-[40vh]">
-        <span className="text-white">Rabbit Hole</span>
+      <div className="w-full h-[120vh] sm:h-[140vh] pt-[40vh]">
+        <div className="w-full flex items-center justify-center mb-4">
+          <span className="text-white text-5xl sm:text-6xl text-center">HIGHLIGHTS</span>
+        </div>
+        <div className="flex h-full w-full overflow-x-auto gap-4 px-10 scrollbar-hidden">
+          {photos.map((photo: string, i: number) => (
+            <div
+              key={i}
+              className="min-w-[90%] w-[90%] sm:min-w-[30%] sm:w-[30%] h-[90%] sm:h-[60%] flex items-center justify-center overflow-hidden shadow-lg bg-black hover:scale-[1.02] transition-all ease-in-out"
+            >
+              <img
+                src={photo}
+                alt=""
+                className="w-full h-full object-cover object-center"
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
