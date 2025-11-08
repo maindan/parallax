ead945c2aff9c6251b81d6c942dcbeb4236b571f
dan <danielkksilva1@gmail.com>
2025-11-08 12:33:35 -0400

    feat: scroll and gsap integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,60 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import Content from './pages/content/Content'
 import 'locomotive-scroll/dist/locomotive-scroll.css'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
+
+gsap.registerPlugin(ScrollTrigger)
 
 export default function App() {
   const scrollRef = useRef<HTMLDivElement | null>(null)
+  const [isReady, setIsReady] = useState(false)
 
   useEffect(() => {
-    let locomotive: any
-
     if (!scrollRef.current) return
+    let locoScroll: any
 
-    (async () => {
+    ;(async () => {
       const LocomotiveScroll = (await import('locomotive-scroll')).default
-      locomotive = new LocomotiveScroll({
+      locoScroll = new LocomotiveScroll({
         el: scrollRef.current!,
         smooth: true,
         lerp: 0.07,
       })
+
+      ScrollTrigger.scrollerProxy(scrollRef.current, {
+        scrollTop(value) {
+          if (arguments.length) locoScroll.scrollTo(value, { duration: 0 })
+          return locoScroll.scroll.instance.scroll.y
+        },
+        getBoundingClientRect() {
+          return {
+            top: 0,
+            left: 0,
+            width: window.innerWidth,
+            height: window.innerHeight,
+          }
+        },
+        pinType: scrollRef.current!.style.transform ? 'transform' : 'fixed',
+      })
+
+      locoScroll.on('scroll', ScrollTrigger.update)
+      ScrollTrigger.addEventListener('refresh', () => locoScroll.update())
+
+      // Marca que o scroll está pronto
+      setIsReady(true)
+
+      setTimeout(() => {
+        ScrollTrigger.refresh()
+      }, 500)
     })()
 
-    return () => {
-      if (locomotive) locomotive.destroy()
-    }
+    return () => locoScroll?.destroy()
   }, [])
 
   return (
     <div data-scroll-container ref={scrollRef}>
-      {/* Passa a referência do pai como prop */}
-      {scrollRef.current && <Content scrollContainer={scrollRef} />}
+      {isReady && <Content scrollContainer={scrollRef} />}
     </div>
   )
 }
--- src/pages/content/Content.tsx
+++ src/pages/content/Content.tsx
@@ -1,67 +1,76 @@
 import gsap from "gsap";
-import { ScrollTrigger } from "gsap/all";
 import { useLayoutEffect, useRef } from "react";
 
-gsap.registerPlugin(ScrollTrigger);
-
 type ContentProps = {
-  scrollContainer: React.RefObject<HTMLDivElement | null>; // <-- permite null
+  scrollContainer: React.RefObject<HTMLDivElement | null>;
 };
 
 export default function Content({ scrollContainer }: ContentProps) {
   const bgRef = useRef<HTMLImageElement | null>(null);
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const photoRef = useRef<HTMLImageElement | null>(null)
 
   useLayoutEffect(() => {
-    console.log(scrollContainer.current)
     if (!scrollContainer.current || !bgRef.current) return;
 
-    console.log('chegou')
-
-    const timeline = gsap.timeline({
+    const tl = gsap.timeline({
       scrollTrigger: {
         trigger: bgRef.current,
         scroller: scrollContainer.current,
         start: "top top",
-        end: "+=200px",
+        end: "+=400px",
         scrub: true,
-        markers: true,
       },
     });
 
-    timeline
-        .from(bgRef.current,
-            { clipPath: "inset(20%)"});
+    tl.fromTo(
+      bgRef.current,
+      { clipPath: "inset(20%)" },
+      { clipPath: "inset(0%)", ease: "none" }
+    );
 
-    return () => timeline.scrollTrigger?.kill();
+    tl.fromTo(
+      photoRef.current,
+      { width: "40%" },
+      { width: "50%" }
+    )
+
+    return () => {
+      tl.scrollTrigger?.kill();
+      tl.kill();
+    };
   }, [scrollContainer]);
 
   return (
-    <div className="h-full w-full bg-black">
+    <div className="h-full w-full bg-black" ref={containerRef}>
       <div
-        className="w-full h-screen relative flex items-center justify-center mt-50"
+        className="w-full h-screen relative flex items-center justify-center mt-40"
       >
         <img
-          src="arch.jpg"
+          src="sky.jpg"
           alt=""
           ref={bgRef}
-          className="h-[140vh] w-full object-cover absolute"
+          className="h-[160vh] w-full object-cover absolute"
         />
         <img
-          src="man.jpg"
+          src="woman.jpg"
           alt=""
           data-scroll
-          data-scroll-speed="0.8"
+          data-scroll-speed="0.1"
           className="w-[15%] z-10"
+          ref={photoRef}
         />
         <h2
           data-scroll
           data-scroll-speed="2"
           className="absolute z-20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-6xl text-white"
         >
-          TEXTOOOOOO
+          PHOTOGRAPH
         </h2>
       </div>
-      <div className="w-full h-screen"></div>
+      <div className="w-full h-[140vh] pt-[40vh]">
+        <span className="text-white">Rabbit Hole</span>
+      </div>
     </div>
   );
 }
